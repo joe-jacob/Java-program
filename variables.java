ddb5f0f4706994f546aa3fc5abd104b4841d8064
joe-jacob <90095203+joe-jacob@users.noreply.github.com>
2021-09-05 15:33:53 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,19 @@
+package assignment1;
+
+public class variables {
+
+	public static void main(String[] args) {
+		int a_int = 5;
+		float b_float = 5.99f;
+		char c_letter = 'D';
+		boolean d_Bool = true;
+		double e_double = 12.3;
+		System.out.println(a_int);
+		System.out.println(b_float);
+		System.out.println(c_letter);
+		System.out.println(d_Bool);
+		System.out.println(e_double);
+		
+	}
+
+}
