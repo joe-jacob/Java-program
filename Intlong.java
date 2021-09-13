71dad5ed4e34f182eb129c274405aea8f4575a12
joe-jacob <90095203+joe-jacob@users.noreply.github.com>
2021-09-13 20:18:38 +0530

    Assignment 2

---
+++ Intlong.java
@@ -0,0 +1,16 @@
+package assignment2;
+
+public class Intlong {
+
+	public static void main(String[] args) {
+		Integer num = 10;
+
+        System.out.println(num);            
+
+        Long numInLong = Long.valueOf(num);
+
+        System.out.println(numInLong);       
+
+	}
+
+}
---
+++ Longfloat.java
@@ -0,0 +1,12 @@
+package assignment2;
+
+public class Longfloat {
+
+	public static void main(String[] args) {
+		Long longObject = new Long("224");
+		 float f = longObject.floatValue();
+		    System.out.println(+f);
+
+	}
+
+}
